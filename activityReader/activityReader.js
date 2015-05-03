f0c17f1557ebcf107135dd13d3bc6f242e9d1556
Jens Rydenheim <jens.rydenheim@softhouse.se>
2015-05-03 10:51:23 +0200

    Only add new activities to the database

--- activityReader/activityReader.js
+++ activityReader/activityReader.js
@@ -8,41 +8,60 @@ var xml2js = require('xml2js');
 var fs = require('fs');
 
 var mongoUrl = 'mongodb://localhost:27017/fitlist';
+var activitiesCollection;
 
-fs.readdir(__dirname + '/data', function (err, files) {
-    files.forEach(function (file) {
-        fs.readFile(__dirname + '/data/' + file, function(err, data) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            var parser = new xml2js.Parser({ attrkey: 'attrKeys',
-                explicitArray: false,
-                mergeAttrs: true,
-                valueProcessors: [xml2js.processors.parseNumbers]
-            });
-            parser.parseString(data, function (err, result) {
+MongoClient.connect(mongoUrl, function(err, db) {
+    activitiesCollection = db.collection('activities');
+    readActivities();
+});
+
+function readActivities() {
+    fs.readdir(__dirname + '/data', function (err, files) {
+        files.forEach(function (file) {
+            fs.readFile(__dirname + '/data/' + file, function (err, data) {
                 if (err) {
                     console.error(err);
+                    return;
                 }
-                console.log(JSON.stringify(result));
-                console.log(result.TrainingCenterDatabase.Activities.Activity.Lap[0].TotalTimeSeconds);
-                insertActivity(result, function () {
-                    console.log('Done inserting into database');
-                });
+                parseActivityFile(data);
             });
         });
     });
-});
+}
+
+function parseActivityFile(data) {
+    var parser = new xml2js.Parser({
+        attrkey: 'attrKeys',
+        explicitArray: false,
+        mergeAttrs: true,
+        valueProcessors: [xml2js.processors.parseNumbers]
+    });
+    parser.parseString(data, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+        var activityId = result.TrainingCenterDatabase.Activities.Activity.Id;
+        isNewActivity(activityId, function () {
+            console.log("Inserting: " + activityId);
+            insertActivity(result);
+        });
+    });
+}
+
+function isNewActivity(id, callback) {
+    activitiesCollection.findOne({'TrainingCenterDatabase.Activities.Activity.Id' : id}, function (err, result) {
+        if (!result) {
+            callback();
+        }
+    });
+}
 
 var insertActivity = function(activity, callback) {
-    MongoClient.connect(mongoUrl, function(err, db) {
-        var collection = db.collection('activities');
-        collection.insert(activity, function(err, result) {
-            if (err) {
-                console.error(err);
-            }
-            callback(result);
-        });
+    activitiesCollection.insert(activity, function(err, result) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Done inserting into database');
+        }
     });
 }
